25f487b52b9e540ec42e3aa04ecf365b5bf2bc8f
Martijn Luyckx <martijnluyckx@Martijns-MacBook-Pro.local>
2018-10-31 14:10:30 +0100

    node express boilerplate

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var MobileDetect = require('mobile-detect');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+// Initialize the session
+app.use(session({
+    store: new FileStore(),
+    secret: 'martijniscool123456',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        // in miliseconds, this is one hour
+        maxAge: 3600000,
+    }
+}));
+
+// Routing for the game
+app.get('/', function(req, res) {
+    // Use this to check if the browser is a mobile one
+    md = new MobileDetect(req.headers['user-agent']);
+
+    // If the browser is not a mobile one, don't allow to play the game
+    if (!md.mobile()) {
+        res.sendFile(`${__dirname}/not-mobile.html`);
+    } else {
+        res.sendFile(`${__dirname}/dotsmash.html`);
+    }
+});
+
+// Socket code for each connected client
+io.on('connection', function(socket){
+    // Log that a client is connected, for debug purposes
+    console.log(`user connected: ${socket.id}`);
+
+    // Log if a client disconnects, for debug purposes
+    socket.on('disconnect', function(){
+        console.log(`user disconnected: ${socket.id}`);
+    });
+});
+
+// Normal HTTP
+// Listen on port 80
+http.listen(80, function(){
+    // Log that we're actually active, for debug purposes
+    console.log('listening on *:80');
+});
